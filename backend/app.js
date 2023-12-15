67de59202d2392196bffe12d7a3c0aedc5ba4334
Taphel <raphael.taffoureau@gmail.com>
2023-12-15 15:26:08 +0100

    fix dburl

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ const crypto = require('crypto');
 const bcrypt = require('bcrypt');
 const mongoSanitize = require('express-mongo-sanitize');
 const sanitizeHtml = require("sanitize-html");
+const dbUrl = process.env.DbUrl;
 
 // Require models
 const Event = require('./models/event');
